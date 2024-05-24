4e212fbc55a08e493332590c372e482e3be5b4d5
ajay1931 <ajayprakash032@gmail.com>
2024-05-24 07:34:48 +0530

    for_loop init

---
+++ num.js
@@ -0,0 +1,13 @@
+
+var i = 0
+for (i > 0; i <= 40; i++) {
+    var even = i % 2 !== 0 ? i :
+        console.log("number " + i)
+}
+
+// var i=0
+// for(i>0;i<=40;i++){
+//     if(i%2===0){
+//         console.log(i)
+//     }
+// }
---
+++ numbers.js
@@ -0,0 +1,15 @@
+// for (var i = 1; i <= 10; i++) {
+//     if (i % 2 !== 0) {
+//         console.log("number " + i)
+//     }
+// }
+
+// for (var i = 10; i >= 1; i--) {
+//     if (i % 2 !== 0) {
+//         console.log("number " + i)
+//     }
+// }
+
+for ( i = 1; i <= 10; i++) {
+        console.log("number " + i)
+}
