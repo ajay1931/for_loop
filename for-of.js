60f8ecd43b78f91e59ba89130fc35efe0add53d2
ajay1931 <ajayprakash032@gmail.com>
2024-06-15 07:01:30 +0530

    for-of init

---
+++ for-of.js
@@ -0,0 +1,16 @@
+// const person = {
+//     name:'ajay',
+//     age:24,
+//     sex:'male'
+// };
+// for(let key of person){
+//     console.log(key);
+// }
+
+// the above code not working
+
+let colours=["red","green","black","orange","brown"]
+for(let key of colours){
+     console.log(key);
+    
+}
