2d1b35ee03974f005c98cf002d5c27b77ba276c0
ajay1931 <ajayprakash032@gmail.com>
2024-06-15 06:45:55 +0530

    for-in init

---
+++ for-in.js
@@ -0,0 +1,16 @@
+const person ={
+    name:'ajay',
+    age:24,
+    sex:'male'
+}; 
+
+for(let key in person){
+    console.log(key + ":" +person[key])
+}
+
+let colours=["red","green","black","orange","brown"]
+for(let key in colours){
+    // console.log(key);
+     console.log(colours[key]);
+    
+}
